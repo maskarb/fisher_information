8f50c53d23d6535c9791b2437de3dcac8facd131
michael skarbek <maskarb@gmail.com>
2019-07-24 10:15:30 -0400

    add reference. format with black

--- discrete_amp_overlap.py
+++ discrete_amp_overlap.py
@@ -1,9 +1,22 @@
+"""
+The size_of_state and fisher_univariate are heavy modifications of code found here:
+
+https://csun.uic.edu/codes/fisher.html
+
+N. Ahmad, S. Derrible, T. Eason, and H. Cabezas, 2016, “Using Fisher information to track stability in multivariate systems”,
+Royal Society Open Science, 3:160582, DOI: 10.1098/rsos.160582
+"""
+
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+
 def cast_x_list(lis):
     return [[i] for i in lis]
+
+
 def give_what_i_need(lis):
     return [x[-1] for x in lis]
 
@@ -14,7 +27,7 @@ def size_of_state(data_num, window_size, k):
     for j in range(len(d_f.columns)):
         sos_temp = []
         for i in d_f.index:
-            A = list(d_f[j][i:i+window_size])
+            A = list(d_f[j][i : i + window_size])
             A_1 = [float(i) for i in A if i != 0]
             if len(A_1) == window_size:
                 sos_temp.append(np.std(A_1, ddof=1))
@@ -30,16 +43,16 @@ def fisher_univariate(x_list, k, w_size, w_incr):
     data_num = cast_x_list(x_list)
     FI_final, k_init = [], []
     for i in range(0, len(data_num), w_incr):
-        data_win = data_num[i:i+w_size]
+        data_win = data_num[i : i + w_size]
         if len(data_win) == w_size:
             _bin = []
             for m in range(w_size):
-                bin_temp=[]
+                bin_temp = []
                 for n in range(w_size):
-                    if m==n:
-                        bin_temp.append('I')
+                    if m == n:
+                        bin_temp.append("I")
                     else:
-                        bin_temp_1=[]
+                        bin_temp_1 = []
                         for k in range(len(data_win[n])):
                             if (abs(data_win[m][k] - data_win[n][k])) <= sost[k]:
                                 bin_temp_1.append(1)
@@ -55,20 +68,24 @@ def fisher_univariate(x_list, k, w_size, w_incr):
                     if j not in bin_2:
                         bin_1_temp = [j]
                         for i in range(len(_bin[j])):
-                            if _bin_val[i] != 'I' and _bin_val[i] >= tl1 and i not in bin_2:
+                            if (
+                                _bin_val[i] != "I"
+                                and _bin_val[i] >= tl1
+                                and i not in bin_2
+                            ):
                                 bin_1_temp.append(i)
                         bin_1.append(bin_1_temp)
                         bin_2.extend(bin_1_temp)
                 prob = [0]
                 for i in bin_1:
-                    prob.append(float(len(i))/len(bin_2))
+                    prob.append(float(len(i)) / len(bin_2))
                 prob.append(0)
                 prob_q = []
                 for i in prob:
                     prob_q.append(np.sqrt(i))
                 FI_temp = 0
                 for i in range(len(prob_q) - 1):
-                    FI_temp += (prob_q[i] - prob_q[i+1]) ** 2
+                    FI_temp += (prob_q[i] - prob_q[i + 1]) ** 2
                 FI_temp = 4 * FI_temp
                 FI.append(FI_temp)
             for i in range(len(FI)):
@@ -78,16 +95,17 @@ def fisher_univariate(x_list, k, w_size, w_incr):
             FI_final.append(FI)
     if not k_init:
         k_init.append(0)
-    for i, FI_val in enumerate(FI_final): #range(0, len(FI_final)):
+    for i, FI_val in enumerate(FI_final):  # range(0, len(FI_final)):
         FI_final[i].append(
-            float(sum(FI_val[min(k_init):len(FI_val)])) /
-            len(FI_val[min(k_init):len(FI_val)])
-            )
+            float(sum(FI_val[min(k_init) : len(FI_val)]))
+            / len(FI_val[min(k_init) : len(FI_val)])
+        )
     return give_what_i_need(FI_final)
 
+
 if __name__ == "__main__":
-    df = pd.read_csv('cantar2019.csv')
-    x = list(df['storage'])
+    df = pd.read_csv("cantar2019.csv")
+    x = list(df["storage"])
 
     k = 2
     dN = 48
